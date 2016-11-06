42bceae4ae9eb7e14b1bd4d46c8e2c84302b6ff6
Hector Partidas <hpartidas@deuz.net>
2016-11-05 21:42:13 -0500

    Added Tests Application.properties

--- initial/src/main/java/hello/Greeting.java
+++ initial/src/main/java/hello/Greeting.java
@@ -1,7 +1,20 @@
 package hello;
 
-/**
- * Created by hpartidas on 11/5/16.
- */
 public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
